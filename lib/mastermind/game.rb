029241c4954966bdf7f9cb8e7b3a642f6df602ef
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-29 17:08:50 +0530

    Add a play method that plays a game of mastermind
    The game has a set of 12 turns, and at each turn a player's guessed code
    and another player's secret code is displayed.

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -1,8 +1,21 @@
 class Game
+  TURNS = 12
+
   attr_reader :code_maker, :code_breaker
+  attr_accessor :secret_code
 
   def initialize(code_maker, code_breaker)
+    @secret_code = ''
     @code_maker = code_maker
     @code_breaker = code_breaker
+  end
+
+  def play
+    self.secret_code = code_maker.generate_code
+    puts "code: #{secret_code}"
+    TURNS.times do |turn|
+      guessed_code = code_breaker.guess_code
+      puts "=> turn: #{turn + 1}, guess: #{guessed_code}"
+    end
   end
 end
