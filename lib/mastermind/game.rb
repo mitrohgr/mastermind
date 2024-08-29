a3c4eb7caf021b1437f7f69cb952d53131522126
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-29 21:15:51 +0530

    Give feedback on how good the guess was each turn
    Feedback is provided in the form of a combination of red and white pegs.
    Red pegs signify that the number of guessed colors are correct and in
    the right position whereas white pegs signify that the number of guessed
    colors are correct but in the wrong position.

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -10,12 +10,32 @@ class Game
     @code_breaker = code_breaker
   end
 
+  def evaluate(secret_code, guessed_code)
+    pegs = { red: 0, white: 0 }
+    guessed_code.chars.each_with_index do |color, idx|
+      next unless secret_code.include?(color)
+
+      if color.match?(secret_code[idx])
+        pegs[:red] += 1
+      else
+        pegs[:white] += 1
+      end
+    end
+    pegs
+  end
+
+  def feedback(pegs)
+    "red pegs: #{pegs[:red]}, white pegs: #{pegs[:white]}"
+  end
+
   def play
     self.secret_code = code_maker.generate_code
     puts "code: #{secret_code}"
     TURNS.times do |turn|
       guessed_code = code_breaker.guess_code
-      puts "=> turn: #{turn + 1}, guess: #{guessed_code}"
+      pegs = evaluate(secret_code, guessed_code)
+      puts "=> turn: #{turn + 1}, guess: #{guessed_code}, #{feedback(pegs)}"
+      break if pegs[:red] == 4
     end
   end
 end
